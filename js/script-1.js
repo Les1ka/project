86a103fb229cff66dccce195a9a5a8bd4da6a41e
Olesya <olesya@gmail.com>
2021-09-25 19:03:28 +0500

    first commit

---
+++ js/script-1.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number =10;
+console.log(number);
+
+const obj = {
+    a: 50
+
+};
+
+obj.a= 10;
+console.log(obj);
+
+console.log(name);
+var name = 'Nikolay';
+
+{
+    let result = 50;
+
+} 
+
+console.log(result);
+
+alert(5);
+[].push('a');
+
---
+++ js/script-2.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string'*9);
+
+const persone = `Nikolay`;
+
+const bool = true;
+
+//console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "Lesika",
+    age: 26,
+    isMarried: true    
+}
+
+//console.log(obj.name);
+console.log(obj["name"]);
+
+let arr = ['plum.png','orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[5]);
+
