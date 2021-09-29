cc16262e54f840c22424cc7e196b4a02dd348450
Olesya <olesya@gmail.com>
2021-09-29 20:33:47 +0500

    занятие 11 первая часть практики

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+const personalMovieDB = {
+
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt ('На сколько оцените его?',''),
+    c = prompt('Один из последнихпросмотренных фильмов?', ''),
+    d = prompt ('На сколько оцените его?','');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
+
+    
